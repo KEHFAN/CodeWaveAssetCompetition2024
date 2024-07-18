7e33c3aa19d4ccb777e79d0a5d6c3a62f7c2d6fe
kehfan <kehfan@yeah.net>
2024-07-18 10:19:00 +0800

    增加native.log解析

---
+++ devTools/src/main/java/com/netease/lowcode/extension/logs/parse/NativeLogParse.java
@@ -0,0 +1,12 @@
+package com.netease.lowcode.extension.logs.parse;
+
+/**
+ * parse logs/biz/origin/native.log
+ */
+public class NativeLogParse {
+
+    public static void main(String[] args) {
+
+    }
+
+}
