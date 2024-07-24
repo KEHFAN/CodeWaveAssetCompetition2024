eef6cdf65aa1a518426285a3862983a428d384f0
kehfan <kehfan@yeah.net>
2024-07-24 15:01:08 +0800

    增加spring组件

--- devTools/src/main/java/com/netease/lowcode/extension/spring/controller/LogController.java
+++ devTools/src/main/java/com/netease/lowcode/extension/spring/controller/LogController.java
@@ -1,7 +1,14 @@
 package com.netease.lowcode.extension.spring.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LogController {
+
+    @GetMapping("/rest/test")
+    public String test() {
+        return "hello";
+    }
+
 }
