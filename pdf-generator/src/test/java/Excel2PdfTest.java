44088c081abe3be1de0f47c9fc432efbe143a3ac
kehfan <kehfan@yeah.net>
2024-08-21 17:00:53 +0800

    修改测试用例

--- pdf-generator/src/test/java/Excel2PdfTest.java
+++ pdf-generator/src/test/java/Excel2PdfTest.java
@@ -1,29 +1,31 @@
 import com.netease.lowcode.pdf.extension.Excel2Pdf;
+import com.netease.lowcode.pdf.extension.structures.CreateByXlsxRequest;
 
 public class Excel2PdfTest {
 
     public static void main(String[] args) {
-        Excel2Pdf.xlsx2pdf(
-                "{\n" +
-                        "    \"name\":\"测试名字\",\n" +
-                        "    \"list\":[\n" +
-                        "        {\n" +
-                        "            \"no\":1,\n" +
-                        "            \"name\":\"项目1\",\n" +
-                        "            \"std\":\"国标1\"\n" +
-                        "        },\n" +
-                        "        {\n" +
-                        "            \"no\":2,\n" +
-                        "            \"name\":\"项目2\",\n" +
-                        "            \"std\":\"国标2\"\n" +
-                        "        },\n" +
-                        "        {\n" +
-                        "            \"no\":3,\n" +
-                        "            \"name\":\"项目3\",\n" +
-                        "            \"std\":\"国标3\"\n" +
-                        "        }\n" +
-                        "    ]\n" +
-                        "}"
-        ,"");
+        String s = "{\n" +
+                "    \"name\":\"测试名字\",\n" +
+                "    \"list\":[\n" +
+                "        {\n" +
+                "            \"no\":1,\n" +
+                "            \"name\":\"项目1\",\n" +
+                "            \"std\":\"国标1\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"no\":2,\n" +
+                "            \"name\":\"项目2\",\n" +
+                "            \"std\":\"国标2\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"no\":3,\n" +
+                "            \"name\":\"项目3\",\n" +
+                "            \"std\":\"国标3\"\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+        CreateByXlsxRequest request = new CreateByXlsxRequest();
+        request.setJsonData(s);
+        Excel2Pdf.xlsx2pdf(request);
     }
 }
