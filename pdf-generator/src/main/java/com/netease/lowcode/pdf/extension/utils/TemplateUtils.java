53fe8262a6586857cbbf4db4a9f87df0af854147
kehfan <kehfan@yeah.net>
2024-08-21 16:32:30 +0800

    处理list数据填充

--- pdf-generator/src/main/java/com/netease/lowcode/pdf/extension/utils/TemplateUtils.java
+++ pdf-generator/src/main/java/com/netease/lowcode/pdf/extension/utils/TemplateUtils.java
@@ -433,9 +433,28 @@ public class TemplateUtils {
     }
 
     public static void main(String[] args) {
-        TemplateUtils.transfer("{\n" +
-                "    \"name\":\"测试名字\"\n" +
-                "}");
+        TemplateUtils.transfer(
+                "{\n" +
+                        "    \"name\":\"测试名字\",\n" +
+                        "    \"list\":[\n" +
+                        "        {\n" +
+                        "            \"no\":1,\n" +
+                        "            \"name\":\"项目1\",\n" +
+                        "            \"std\":\"国标1\"\n" +
+                        "        },\n" +
+                        "        {\n" +
+                        "            \"no\":2,\n" +
+                        "            \"name\":\"项目2\",\n" +
+                        "            \"std\":\"国标2\"\n" +
+                        "        },\n" +
+                        "        {\n" +
+                        "            \"no\":3,\n" +
+                        "            \"name\":\"项目3\",\n" +
+                        "            \"std\":\"国标3\"\n" +
+                        "        }\n" +
+                        "    ]\n" +
+                        "}"
+        );
 
     }
 }
