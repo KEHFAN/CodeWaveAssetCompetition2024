68466db7cd5f727ccef629da36d3147354f19a94
kehfan <kehfan@yeah.net>
2024-10-23 15:12:56 +0800

    处理url中文编码

--- video-server/src/main/java/com/netease/lowcode/extension/video/spring/utils/FileUtil.java
+++ video-server/src/main/java/com/netease/lowcode/extension/video/spring/utils/FileUtil.java
@@ -1,20 +1,52 @@
 package com.netease.lowcode.extension.video.spring.utils;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class FileUtil {
 
     public static InputStream getFileInputStream(String urlStr) throws IOException {
-        // TODO：url中包含中文，可能会400错误，需要对url进行编码处理
-        // 目前 低版本ide 生成的url 包含中文
+        // url中包含中文，可能会400错误，需要对url进行编码处理
+        // 目前 低版本ide(3.8) 生成的url 包含中文
         //     高版本ide 文件名放到的query参数中，不会有影响
+
+        // 仅处理中文
         URL url = new URL(urlStr);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        String path = url.getPath();
+        String[] split = path.split("/");
+        StringBuilder sb = new StringBuilder();
+        for (String s : split) {
+            sb.append("/").append(URLEncoder.encode(s, StandardCharsets.UTF_8.name()));
+        }
+        if (StringUtils.isNotBlank(url.getQuery())) {
+
+            sb.append("?");
+            String[] params = url.getQuery().split("&");
+            for (String param : params) {
+                String[] entry = param.split("=");
+                String key = entry[0];
+                String val = entry[1];
+
+                if (!sb.toString().endsWith("?")) {
+                    sb.append("&");
+                }
+
+                sb.append(URLEncoder.encode(key, StandardCharsets.UTF_8.name()));
+                sb.append("=");
+                sb.append(URLEncoder.encode(val, StandardCharsets.UTF_8.name()));
+            }
+
+        }
+        URL newUrl = new URL(url.getProtocol(), url.getHost(), url.getPort(), sb.toString().replace("//","/"));
+        HttpURLConnection connection = (HttpURLConnection) newUrl.openConnection();
         connection.setConnectTimeout(3 * 1000);
         connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36");
-        return url.openStream();
+        return newUrl.openStream();
     }
 
     public static void saveFile(String url, String filename) throws IOException {
--- video-server/src/test/java/util/MainTest.java
+++ video-server/src/test/java/util/MainTest.java
@@ -1,12 +1,35 @@
 package util;
 
+import com.netease.lowcode.extension.video.spring.utils.FileUtil;
 import com.netease.lowcode.extension.video.spring.utils.StringGenerator;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 public class MainTest {
-    public static void main(String[] args) {
+    public static void main1(String[] args) throws UnsupportedEncodingException, MalformedURLException, URISyntaxException {
         System.out.println(UUID.randomUUID());
         System.out.println(StringGenerator.generator(10));
+        System.out.println(StandardCharsets.UTF_8.displayName());
+
+        String url = "https://dev-tianyuan38test-defaulttenant.lcap.codewave-test.163yun.com:443/upload/app/fb5e7ed0-4422-45f8-b862-5fbd6e1dfd99/tDE90eE8_9194231697_uhd-副本_20241023101744091.mp4";
+        System.out.println(URLEncoder.encode(url));
+        System.out.println(URLEncoder.encode(url,StandardCharsets.UTF_8.name()));
+
+        URL uuu = new URL(url);
+        System.out.println(uuu.toURI());
+    }
+
+    public static void main(String[] args) throws IOException {
+        //String url = "https://dev-tianyuan38test-defaulttenant.lcap.codewave-test.163yun.com:443/upload/app/fb5e7ed0-4422-45f8-b862-5fbd6e1dfd99/tDE90eE8_9194231697_uhd-副本_20241023101744091.mp4";
+        //String url2 = "https://dev-tianyuan38test-defaulttenant.lcap.codewave-test.163yun.com:443/upload/app/fb5e7ed0-4422-45f8-b862-5fbd6e1dfd99/tDE90eE8_9194231697_uhd-%E5%89%AF%E6%9C%AC_20241023101744091.mp4";
+        String url = "https://dev-videotest1-kehfan.app.codewave.163.com:443/upload/?fileName=tDE90eE8_9194231697_uhd-副本.mp4&fut=1729664619161&ai=779efa5d-42f3-4fbc-9ac5-7bfdaece528e&con=lcap_default_connection";
+        FileUtil.getFileInputStream(url);
     }
 }
