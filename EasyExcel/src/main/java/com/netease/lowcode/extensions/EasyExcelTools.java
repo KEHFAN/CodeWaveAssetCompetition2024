4ca0f72b051ce46bf5f18fcd958fc3fb29e1e844
kehfan <kehfan@yeah.net>
2024-12-02 13:58:10 +0800

    解析增加参数maxCacheActivateBatchCount

--- EasyExcel/src/main/java/com/netease/lowcode/extensions/EasyExcelTools.java
+++ EasyExcel/src/main/java/com/netease/lowcode/extensions/EasyExcelTools.java
@@ -3,6 +3,7 @@ package com.netease.lowcode.extensions;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.cache.selector.SimpleReadCacheSelector;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.read.metadata.ReadSheet;
@@ -375,8 +376,13 @@ public class EasyExcelTools {
         }
 
         LibraryReadListener<?> readListener = new LibraryReadListener<>(handle, clazz, request, fileUtils);
+        SimpleReadCacheSelector simpleReadCacheSelector = new SimpleReadCacheSelector();
+        // 放多少批数据在内存，默认20
+        if (Objects.nonNull(request.getMaxCacheActivateBatchCount())) {
+            simpleReadCacheSelector.setMaxCacheActivateBatchCount(request.getMaxCacheActivateBatchCount());
+        }
         try (InputStream inputStream = openUrlStream(request.getUrl())) {
-            EasyExcel.read(inputStream, clazz, readListener).sheet().doRead();
+            EasyExcel.read(inputStream, clazz, readListener).readCacheSelector(simpleReadCacheSelector).sheet().doRead();
         } catch (RuntimeException e) {
             log.error("excel解析失败", e);
             return ParseBigDataResponse.FAIL("excel解析失败！", e.getMessage() + Arrays.toString(e.getStackTrace()));
